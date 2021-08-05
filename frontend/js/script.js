6b1e348a0ca683e1da9d17b56881830e92d79177
Kirill <francesowo@gmail.com>
2021-08-06 01:56:16 +0300

    lock loading

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -2,7 +2,7 @@ let meowButton = document.querySelector(".meow-button");
 let catImg = document.querySelector(".cat-img");
 let description = document.querySelector(".description");
 let randomButton = document.querySelector(".random-button");
-
+let isLoading = false;
 
 meowButton.addEventListener("click", function() {
   fetch("http://localhost:8080/cat")
@@ -14,14 +14,19 @@ meowButton.addEventListener("click", function() {
 })
 
 randomButton.addEventListener("click", function() {
+  if (isLoading) {
+    return
+  };
   catImg.src = "/images/loading.gif";
   description.textContent = "Котик загружается";
+  isLoading = true;
   fetch("https://api.thecatapi.com/v1/images/search")
   .then(response => response.json())
   .then(data => {
     catImg.src = data[0].url;
     catImg.onload = function() {
       description.textContent = "Вы вытащили рандомного котика";
+      isLoading = false;
       catImg.onload = undefined;
     }
   })
