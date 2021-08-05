06ba3b60d694c63a6fe544aa1f512a3ccc637870
Kirill <francesowo@gmail.com>
2021-08-06 01:48:42 +0300

    add function random cat

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,8 +1,10 @@
-let button = document.querySelector(".meow-button");
+let meowButton = document.querySelector(".meow-button");
 let catImg = document.querySelector(".cat-img");
 let description = document.querySelector(".description");
+let randomButton = document.querySelector(".random-button");
 
-button.addEventListener("click", function() {
+
+meowButton.addEventListener("click", function() {
   fetch("http://localhost:8080/cat")
     .then(response => response.json())
     .then(data => {
@@ -10,3 +12,17 @@ button.addEventListener("click", function() {
       description.textContent = data.description;
     });
 })
+
+randomButton.addEventListener("click", function() {
+  catImg.src = "/images/loading.gif";
+  description.textContent = "Котик загружается";
+  fetch("https://api.thecatapi.com/v1/images/search")
+  .then(response => response.json())
+  .then(data => {
+    catImg.src = data[0].url;
+    catImg.onload = function() {
+      description.textContent = "Вы вытащили рандомного котика";
+      catImg.onload = undefined;
+    }
+  })
+});
